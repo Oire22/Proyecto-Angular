ebcebee4a948b5ddff68fcde599349550b466b13
Oire22 <oliveriomaranesi@gmail.com>
2023-01-03 14:54:32 -0300

    Angular Front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 
 //mis componentes
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
-  {path: "about", component: AboutComponent}
+  {path: "header", component: HeaderComponent},
+  {path: "acerca-de", component: AcercaDeComponent},
+  {path: "educacion", component: EducacionComponent},
+  {path: "experiencia", component: ExperienciaComponent},
+  {path: "proyectos", component: ProyectosComponent},
+  {path: "footer", component: FooterComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//importar modulo de formulario reactivo
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    FooterComponent,
+
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, //cargar modulos al proyecto
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
